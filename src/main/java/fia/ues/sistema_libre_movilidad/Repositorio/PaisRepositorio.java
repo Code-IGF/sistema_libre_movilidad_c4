a34e0c8f67a91cf3430e9afcc8ffe7a5da0ab446
xenia-01 <111935046+xenia-01@users.noreply.github.com>
2022-10-11 16:00:19 -0600

    Pais

---
+++ src/main/java/fia/ues/sistema_libre_movilidad/Repositorio/PaisRepositorio.java
@@ -0,0 +1,12 @@
+package fia.ues.sistema_libre_movilidad.Repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fia.ues.sistema_libre_movilidad.Entidad.Pais;
+
+public interface PaisRepositorio extends JpaRepository<Pais, Long>{
+
+}
+
+    
+
