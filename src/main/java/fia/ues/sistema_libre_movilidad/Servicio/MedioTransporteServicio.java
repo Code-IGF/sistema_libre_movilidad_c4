447c596c05109eb40786ca817169e2aa884c066b
Marvin-Alexander <cc17043@ues.edu.sv>
2022-10-12 18:19:23 -0600

    Medio-Transporte 1.2

--- src/main/java/fia/ues/sistema_libre_movilidad/Servicio/MedioTransporteServicio.java
+++ src/main/java/fia/ues/sistema_libre_movilidad/Servicio/MedioTransporteServicio.java
@@ -6,4 +6,12 @@ import fia.ues.sistema_libre_movilidad.Entidad.MedioTransporte;
 
 public interface MedioTransporteServicio {
     public List<MedioTransporte> ListarMedioTransporte();
+
+    public MedioTransporte guardarMedioTransporte(MedioTransporte medioTransporte);
+
+    public MedioTransporte obtenerMedioTransporteporId(Long Id);
+
+    public MedioTransporte actualizarMedioTransporte(MedioTransporte medioTransporte);
+
+    public void eliminarMedioTransporte(Long Id); 
 }
