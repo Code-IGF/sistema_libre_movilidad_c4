52a054f80e9ecd294dc0fc4278be80534e9cfa06
MarvinSMartinez <71672000+MarvinSMartinez@users.noreply.github.com>
2022-10-17 20:00:32 -0600

    Cambios en referencias

--- src/main/java/fia/ues/sistema_libre_movilidad/Controlador/Acceso.java
+++ src/main/java/fia/ues/sistema_libre_movilidad/Controlador/Acceso.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Acceso {
-    @GetMapping({""})
+    
+    @GetMapping({"home"})
     public String start(){
-        return "vista_inicial";
+        return "home";
     }
 
-    @GetMapping("login")
+    @GetMapping("")
     public String login()
     {
         return "login";
