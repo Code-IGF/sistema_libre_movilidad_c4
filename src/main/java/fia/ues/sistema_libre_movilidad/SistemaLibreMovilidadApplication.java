0e6cd7b1a60db5d27be6297d05240ff16119905b
ronaldo-martinez <irmv_0311@hotmial.com>
2022-10-18 04:00:37 -0600

    login y home

--- src/main/java/fia/ues/sistema_libre_movilidad/SistemaLibreMovilidadApplication.java
+++ src/main/java/fia/ues/sistema_libre_movilidad/SistemaLibreMovilidadApplication.java
@@ -2,9 +2,15 @@ package fia.ues.sistema_libre_movilidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SistemaLibreMovilidadApplication {
+public class SistemaLibreMovilidadApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SistemaLibreMovilidadApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaLibreMovilidadApplication.class, args);
