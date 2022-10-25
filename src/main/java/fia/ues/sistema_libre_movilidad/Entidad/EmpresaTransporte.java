6237f594aa5075224c0652447242aa32fe7cf7aa
Jakelin <jakita8889@gmail.com>
2022-10-25 00:55:32 -0600

    Tarea 02 empresa transporte

--- src/main/java/fia/ues/sistema_libre_movilidad/Entidad/EmpresaTransporte.java
+++ src/main/java/fia/ues/sistema_libre_movilidad/Entidad/EmpresaTransporte.java
@@ -4,7 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "empresa_transporte")
@@ -13,9 +16,20 @@ public class EmpresaTransporte {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     @Column(name = "nombre", nullable = false, length = 50)
     private String nombre;
 
+    @OneToOne
+    @JoinColumn(name="id_usuario")
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
     public Long getId(){
         return id;
     }
@@ -32,9 +46,14 @@ public class EmpresaTransporte {
         this.nombre=nombre;
     }
 
-    public EmpresaTransporte(Long id,String nombre) {
+    public EmpresaTransporte(Long id,String nombre,Usuario usuario) {
         this.id = id;
         this.nombre = nombre;
+        this.usuario=usuario;
+    }
+    public EmpresaTransporte(String nombre,Usuario usuario) {
+        this.nombre = nombre;
+        this.usuario=usuario;
     }
     public EmpresaTransporte(){
         
