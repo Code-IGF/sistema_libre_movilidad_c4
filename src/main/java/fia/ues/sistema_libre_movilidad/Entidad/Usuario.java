5d326a75fdde02d4478befa690c41cc1eaace323
ronaldo-martinez <irmv_0311@hotmail.com>
2022-11-19 22:46:32 -0600

    ya funciona el regitro y se agregaron los parametros de viajero

--- src/main/java/fia/ues/sistema_libre_movilidad/Entidad/Usuario.java
+++ src/main/java/fia/ues/sistema_libre_movilidad/Entidad/Usuario.java
@@ -17,6 +17,26 @@ public class Usuario {
     private Long id;
 
     @NotEmpty
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+
+    @NotEmpty
+    @Column(name = "apellido", nullable = false, length = 50)
+    private String apellido;
+
+    @NotEmpty
+    @Column(name = "fecha_nacimiento", nullable = false, length = 10)
+    private String fechaNacimiento;
+    
+    @NotEmpty
+    @Column(name = "sexo", nullable = false, length = 1)
+    private String sexo;
+
+    @NotEmpty
+    @Column(name = "telefono", nullable = false, length = 10)
+    private String telefono;
+
+    @NotEmpty
     @Column(name = "correo", unique = true ,nullable = false)
     private String correo;
 
@@ -27,15 +47,17 @@ public class Usuario {
     public Usuario() {        
     }
 
-    public Usuario(Long id, String correo, String contrasenia) {
+    public Usuario(Long id, String nombre, String apellido, String fechaNacimiento, String sexo, String telefono, String correo, String contrasenia) {
         this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.telefono = telefono;
         this.correo = correo;
         this.contrasenia = contrasenia;
     }
-    public Usuario(String correo) {
-        this.correo = correo;
-    }
-
+    
 
     public Long getId(){
         return id;
@@ -60,4 +82,44 @@ public class Usuario {
     public void setContrasenia(String contrasenia){
         this.contrasenia = contrasenia;
     }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
 }
